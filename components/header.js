f5e8d55a8ff4c604e722634d4e82044ef6cf2fa1
fernando_n_dev <tierragraphics@gamil.com>
2022-07-30 15:54:02 -0700

    Header section

--- components/header.js
+++ components/header.js
@@ -1,23 +1,51 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from 'next/image';
 import React from 'react';
 // import CV from '../public/assets/cv.pdf';
+import ME from '../../assets/me.png';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 
 export default function Header() {
   return (
-    <header>
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>First Last</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <div className="cta">
-          {/* <a href={CV} download className="btn">
+    <div className="h-screen text-center relative pt-28 overflow-hidden">
+      <h5>Hello I'm</h5>
+      <h1>First Last</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <div className="flex gap-5 mt-10 justify-center">
+        {/* <a href={CV} download className="btn">
             Download CV
           </a> */}
-          <a className="btn">Download CV</a>
-          <a href="#contact" className="btn btn-primary">
-            Let's talk
-          </a>
-        </div>
+        <a className="btn">Download CV</a>
+        <a href="#contact" className="btn btn-primary">
+          Let's talk
+        </a>
       </div>
-    </header>
+
+      <div id="image" className="absolute mt-16 rounded-t-full overflow-hidden">
+        <Image src={ME} alt="me" />
+      </div>
+
+      <a
+        href="#contact"
+        id="scroll__down"
+        className="absolute rotate-90 right-0 bottom-12"
+      >
+        Scroll Down
+      </a>
+
+      <div
+        id="header-socials"
+        className="flex flex-col items-center gap-3 absolute left-0 bottom-12 "
+      >
+        <a href="https://linkedin.com" target="__blank">
+          <BsLinkedin />
+        </a>
+        <a href="https://Github.com" target="__blank">
+          <FaGithub />
+        </a>
+        <div className=" h-8 w-0.5 bg-primary " />
+      </div>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        background: '#1f1f38',
+        backgroundB: '#2c2c6c',
         primary: '#4db5ff',
         secondary: 'rgba(77, 181, 255, 0.4)',
         light: 'rgba(255, 255, 255, 0.6)',
-        //       --color-bg: '#1f1f38',
-        // --color-bg-variant: '#2c2c6c',
-        // --color-primary: '#4db5ff',
-        // --color-primary-variant: 'rgba(77, 181, 255, 0.4)',
-        // --color-white: '#fff',
-        // --color-light: 'rgba(255, 255, 255, 0.6)',
       },
     },
   },
